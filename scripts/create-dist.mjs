f826d44a7ed23633b5fd6986599658cc61a40150
fmoloshnikov <fmoloshnikov@gmail.com>
2025-06-30 17:15:40 +0300

    [SJG-36] fix package.dist.json exports

--- scripts/create-dist.mjs
+++ scripts/create-dist.mjs
@@ -9,5 +9,5 @@ const __dirname = dirname(__filename);
 
 const distPackagePath = path.resolve(__dirname, "../package.dist.json");
 const distDirectoryPath = path.resolve(__dirname, "../dist/package.json");
-fs.cpSync(path.resolve(__dirname, "../build"), path.resolve(__dirname, "../dist"), {recursive: true});
+fs.cpSync(path.resolve(__dirname, "../build"), path.resolve(__dirname, "../dist/build"), {recursive: true});
 fs.copyFileSync(distPackagePath, distDirectoryPath);
