c18279397214557cd3ddc4ecaee5a89ce133843a
fmoloshnikov <fmoloshnikov@gmail.com>
2025-07-07 15:30:01 +0300

    copy version form source package.json

--- scripts/create-dist.mjs
+++ scripts/create-dist.mjs
@@ -6,9 +6,16 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+fs.cpSync(path.resolve(__dirname, "../build"), path.resolve(__dirname, "../dist/build"), {recursive: true});
 
 const distPackagePath = path.resolve(__dirname, "../package.dist.json");
 const distDirectoryPath = path.resolve(__dirname, "../dist/package.json");
-fs.cpSync(path.resolve(__dirname, "../build"), path.resolve(__dirname, "../dist/build"), {recursive: true});
-fs.copyFileSync(distPackagePath, distDirectoryPath);
+// fs.copyFileSync(distPackagePath, distDirectoryPath);
+const sourceJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../package.json")));
+const distJson = JSON.parse(fs.readFileSync(distDirectoryPath));
+distJson.version = sourceJson.version;
+/* eslint-disable no-console */
+console.log(distJson);
+/* eslint-enable no-console */
+fs.writeFileSync(distDirectoryPath, JSON.stringify(distJson, null, 2));
 fs.copyFileSync(path.resolve(__dirname, "../.npmrc"), path.resolve(__dirname, "../dist/.npmrc"));
