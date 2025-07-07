d08c6c546126a5f0e051cc61a1178294997854ca
fmoloshnikov <fmoloshnikov@gmail.com>
2025-07-07 15:38:54 +0300

    fix create-dist script

--- scripts/create-dist.mjs
+++ scripts/create-dist.mjs
@@ -3,19 +3,16 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
+function createDistJson() {
+  const sourceJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../package.json")));
+  const distJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../package.dist.json")));
+  distJson.version = sourceJson.version;
+  fs.writeFileSync(path.resolve(__dirname, "../dist/package.json"), JSON.stringify(distJson, null, 2));
+}
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 fs.cpSync(path.resolve(__dirname, "../build"), path.resolve(__dirname, "../dist/build"), {recursive: true});
-
-const distPackagePath = path.resolve(__dirname, "../package.dist.json");
-const distDirectoryPath = path.resolve(__dirname, "../dist/package.json");
-// fs.copyFileSync(distPackagePath, distDirectoryPath);
-const sourceJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../package.json")));
-const distJson = JSON.parse(fs.readFileSync(distDirectoryPath));
-distJson.version = sourceJson.version;
-/* eslint-disable no-console */
-console.log(distJson);
-/* eslint-enable no-console */
-fs.writeFileSync(distDirectoryPath, JSON.stringify(distJson, null, 2));
+createDistJson();
 fs.copyFileSync(path.resolve(__dirname, "../.npmrc"), path.resolve(__dirname, "../dist/.npmrc"));
